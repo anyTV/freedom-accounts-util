13b70e2a0dfa8f4f591f2497ba5c7ec1471c17b6
Jan Keith Certeza Darunday <jkdarunday@freedom.tm>
2017-12-07 18:53:12 +0800

    Add implementation of cache and token generation

--- index.js
+++ index.js
@@ -1,3 +1,96 @@
 'use strict';
 
-module.exports = {};
+const cudl = require('cuddle');
+const moment = require('moment');
+const _ = require('lodash');
+
+module.exports = {
+    configure,
+    generate_token,
+    clear_server_cache,
+    clear_client_cache,
+    clear_cache
+};
+
+let cache = {
+    server: {},
+    client: {}
+};
+
+const config = {
+    base_url: '',
+    path: '',
+    client_id: '',
+    client_secret: ''
+};
+
+function _cache_get(category, key, fallback = null) {
+    const cache_record = cache[category][key];
+
+    if(cache_record) {
+        if(!moment(cache_record.expiration).isBefore(moment())) {
+            return cache_record.value;
+        }
+
+        delete cache[category][key];
+    }
+
+    return fallback;
+}
+
+function _cache_set(category, key, value, expiry) {
+    cache[category][key] = {
+        expiration: moment().add(expiry, 'seconds'),
+        value: value
+    };
+}
+
+function configure (new_config) {
+    _.merge(config, new_config);
+
+    if(config.client_expiry === null) {
+        config.client_expiry = 600;
+    }
+
+    if(config.server_expiry === null) {
+        config.server_expiry = 600;
+    }
+}
+
+function generate_token (scopes) {
+    const scopes_string = _(scopes).sort().sortedUniq().join(' ');
+    const cached_token = _cache_get('client', scopes_string);
+    const payload = {
+        client_id: config.client_id,
+        client_secret: config.client_secret,
+        grant_type: 'client_credentials',
+        scopes: scopes_string
+    };
+
+    if (!cached_token) {
+        return cudl.post
+            .to(config.base_url + config.path + '/oauth/token')
+            .send(payload)
+            .promise()
+            .then(result => {
+                _cache_set('client', scopes_string, result.access_token, config.client_expiry);
+
+                return result.access_token;
+            });
+    }
+
+    return Promise.resolve(cached_token);
+}
+
+function clear_server_cache () {
+    cache.server = {};
+}
+
+function clear_client_cache () {
+    cache.client = {};
+}
+
+function clear_cache () {
+    clear_server_cache();
+    clear_client_cache();
+}
--- test/index.js
+++ test/index.js
@@ -1,7 +1,91 @@
 'use strict';
 
-const accounts = require('./');
+const accounts = require('../index.js');
 
 const chai = require('chai');
 const chai_as_promised = require('chai-as-promised');
 const nock = require('nock');
+
+chai.should();
+chai.use(chai_as_promised);
+
+const configuration = {
+    base_url: 'https://accounts.freedom.tm',
+    path: '/api/v2',
+    client_id: 'devtest',
+    client_secret: 'devtest'
+};
+
+const scopes = ['https://frnky.api.tm/cms'];
+
+describe('accounts client', () => {
+    const payload = {
+        client_id: configuration.client_id,
+        client_secret: configuration.client_secret,
+        scopes: scopes.join(' '),
+        grant_type: 'client_credentials'
+    };
+
+    accounts.configure(configuration);
+
+    it('should get access token with specified scope', () => {
+            let accounts_nock = nock(configuration.base_url)
+                .post(configuration.path + '/oauth/token', payload)
+                .reply(200, {access_token: 'blabla'});
+
+            accounts.configure({client_expiry: 0});
+
+            return accounts.generate_token(scopes)
+                .then(result => {
+                    result.should.be.a('string');
+                    accounts_nock.isDone().should.equal(true);
+                });
+    });
+
+
+
+    it('should get expired access token again with specified scope', () => {
+        let accounts_nock = nock(configuration.base_url)
+            .post(configuration.path + '/oauth/token', payload)
+            .reply(200, {access_token: 'blabla'});
+
+        accounts.configure({client_expiry: 600});
+
+        return accounts.generate_token(scopes)
+            .then(result => {
+                result.should.be.a('string');
+                accounts_nock.isDone().should.equal(true);
+            });
+    });
+
+
+    it('should get cached access token with specified scope', () => {
+        let accounts_nock = nock(configuration.base_url)
+            .post(configuration.path + '/oauth/token', payload)
+            .reply(200, {access_token: 'blabla'});
+
+        return accounts.generate_token(scopes)
+            .then(result => {
+                result.should.be.a('string');
+                accounts_nock.isDone().should.not.equal(true);
+
+                nock.cleanAll();
+            });
+    });
+
+    it('should not get cached access token when cached is cleared', () => {
+        let accounts_nock = nock(configuration.base_url)
+            .post(configuration.path + '/oauth/token', payload)
+            .reply(200, {access_token: 'blabla'});
+
+        accounts.clear_cache();
+
+        return accounts.generate_token(scopes)
+            .then(result => {
+                result.should.be.a('string');
+                accounts_nock.isDone().should.equal(true);
+
+                nock.cleanAll();
+            });
+    });
+});
