34dd958c0c4c5a5e146a05bec8e03ab493948fdd
Jan Keith Certeza Darunday <jkdarunday@freedom.tm>
2017-12-08 11:20:45 +0800

    Added implementation for verification middleware for required scopes

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const _ = require('lodash');
 
 module.exports = {
     configure,
+    verify_scopes,
     generate_token,
     clear_server_cache,
     clear_client_cache,
@@ -55,6 +56,71 @@ function configure (new_config) {
     if (config.server_expiry === null) {
         config.server_expiry = 600;
     }
+}
+
+function verify_scopes (required_scopes) {
+    return (req, res, next) => {
+        return check_scopes(req, required_scopes)
+            .then(result => {
+                req.client_id = result.client_id;
+                next();
+            })
+            .catch(error => next(error));
+    };
+}
+
+function check_scopes (request, required_scopes) {
+    return check_input(request.headers['access-token'])
+        .then(get_token_info)
+        .then(result => ({result, required_scopes}))
+        .then(check_token_validity);
+}
+
+function check_input (access_token) {
+    return new Promise((accept, reject) => {
+        if (!access_token) {
+            return reject(new Error('Access token is missing.'));
+        }
+
+        accept(access_token);
+    });
+}
+
+function get_token_info (access_token) {
+    const cached_token_info_result = _cache_get('client', access_token);
+
+    if (!cached_token_info_result) {
+        return cudl.get
+            .to(config.base_url + config.path + '/oauth/tokeninfo')
+            .send({access_token: access_token})
+            .promise()
+            .then(result => {
+                _cache_set('server', access_token, result, config.server_expiry);
+
+                return result;
+            });
+    }
+
+    return Promise.resolve(cached_token_info_result);
+}
+
+function check_token_validity ({result, required_scopes}) {
+    return new Promise((resolve, reject) => {
+        if (!result.scopes) {
+            return reject(new Error('Something went wrong, server did not return scopes, please try again.'));
+        }
+
+        const result_scopes = result.scopes.split(' ');
+        const has_valid_scopes = _.find(required_scopes, scope => _.includes(result_scopes, scope));
+
+        if (has_valid_scopes) {
+            return resolve(result);
+        }
+
+        reject(new Error(
+            `You need at least one of the ff. scopes to access this endpoint: ${required_scopes.join(', ')}`
+        ));
+    });
 }
 
 function generate_token (scopes) {
